b2de633dbfc9744c879f2f59734531d012a5d2a0
Charly Coussot <charly.coussot@univ-grenoble-alpe>
2019-10-24 13:54:02 +0200

    feat disable csrf

---
+++ src/main/java/fr/theia_land/in_situ/dataportal/CustomConfig/ApplicationSecurityConfig.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.theia_land.in_situ.dataportal.CustomConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author coussotc
+ */
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf().disable();
+    }
+}
